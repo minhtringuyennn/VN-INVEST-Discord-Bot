820428c44561e908161860ad8bd0f01ca5e05718
minhtringuyennn <20120219@student.hcmus.edu.vn>
2022-01-28 04:37:39 +0700

    Update database function

--- stock_modules/database.py
+++ stock_modules/database.py
@@ -7,16 +7,57 @@ class mongoDB():
     def __init__(self) -> None:
         self.__client = pymongo.MongoClient("mongodb://admin:Bachtam2001@127.0.0.1:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false")
         self.__db = self.__client['stocks']
-        self.__price = self.__db.get_collection('realtime_price')
+        self.__rtprice = self.__db.get_collection('realtime_price')
+        self.__hprice = self.__db.get_collection('history_price')
 
-    def stockUpdate(self,stockNo,stockobj,overwrite=False):
-        key = {'stockNo': stockNo}
+    def stockRealtimeUpdate(self, stockobj, overwrite=False, stockNo = None, code = None):
+        if (stockNo != None and code != None):
+            key = {'stockNo': stockNo,'code':code}
+        elif (stockNo != None):
+            key = {'stockNo': stockNo}
+        elif (code != None):
+            key = {'code':code}
+        else:
+            return
         data = stockobj.__dict__
         if (overwrite):
-            self.__price.update_one(key, {"$set" : data }, upsert=True)
+            self.__rtprice.update_one(key, {"$set" : data }, upsert=True)
         else:
-            self.__price.update_one(key, {"$setOnInsert" : data}, upsert=True)
+            self.__rtprice.update_one(key, {"$setOnInsert" : data}, upsert=True)
 
-    def stockRead(self,stockNo):
-        key = {'stockNo': stockNo}
-        return self.__price.find_one(key)
+    def stockUpdate(self, stockobj, overwrite=False, stockNo = None, code = None):
+        if (stockNo != None and code != None):
+            key = {'stockNo': stockNo,'code':code}
+        elif (stockNo != None):
+            key = {'stockNo': stockNo}
+        elif (code != None):
+            key = {'code':code}
+        else:
+            return
+        data = stockobj.__dict__
+        if (overwrite):
+            self.__hprice.update_one(key, {"$set" : data }, upsert=True)
+        else:
+            self.__hprice.update_one(key, {"$setOnInsert" : data}, upsert=True)
+
+    def stockRealtimeRead(self,stockNo = None, code = None):
+        if (stockNo != None and code != None):
+            key = {'stockNo': stockNo,'code':code}
+        elif (stockNo != None):
+            key = {'stockNo': stockNo}
+        elif (code != None):
+            key = {'code':code}
+        else:
+            return None
+        return self.__rtprice.find_one(key)    
+
+    def stockRead(self,stockNo = None, code = None):
+        if (stockNo != None and code != None):
+            key = {'stockNo': stockNo,'code':code}
+        elif (stockNo != None):
+            key = {'stockNo': stockNo}
+        elif (code != None):
+            key = {'code':code}
+        else:
+            return None
+        return self.__hprice.find_one(key)
